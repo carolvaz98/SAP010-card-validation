10f9d5f928bce7d0ca2bc3ac938e1521c39dd2ed
Laboratoria/SAP010-card-validation <carolvas98@hotmail.com>
2023-04-20 20:10:41 -0300

    Primeiro Projeto

--- src/index.js
+++ src/index.js
@@ -2,26 +2,26 @@ import validator from './validator.js';
 
 console.log(validator);
 
-// Cria uma matriz de números
+//Cria uma matriz de números
 const numeros = [0,1,2,3,4,5,6,7,8,9];
 
 // Cria uma nova matriz vazia para armazenar os números pares multiplicados por 2
 const numerosParesMultiplicados = [];
 
-// Loop for para percorrer todos os números na matriz original
+//Loop for para percorrer todos os números na matriz original
 for (let i = 0; i < numeros.length; i++) {
 
 const numero = numeros[i]
 
-// Verifica se o número é par usando o operador módulo (%)
+//Verifica se o número é par usando o operador módulo (%)
 if (numero % 2 === 0) {
 
-// Se o número for par, multiplique-o por 2 e adicione-o à nova matriz
-    const numeroMultiplicado = numero * 2;
+//Se o número for par, multiplique-o por 2 e adicione-o à nova matriz
+const numeroMultiplicado = numero * 2
 
-    numerosParesMultiplicados.push(numeroMultiplicado);
-    
+numerosParesMultiplicados.push(numeroMultiplicado);
+
+console.log(numerosParesMultiplicados);
+  }
 }
-    
-    
-}
+
